377bab471f7f1a41ee966d93cec7a881cb592e8c
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2025-03-02 02:58:47 +0900

    feat: Init backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { Sequelize } = require('sequelize')
+// we'll refactor later ....😂
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const { Sequelize, QueryTypes } = require('sequelize')
 const conn_info = {
     'host': process.env.DUCKDB_HOST,
     'port': process.env.DUCKDB_PORT,
@@ -12,40 +16,26 @@ const conn_info = {
 }
 const sequelize = new Sequelize(options=conn_info)
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-const main = async() => {
-    try {
-        await sequelize.authenticate()
-        console.log("connection established1")
-        sequelize.close()
-    } catch (error) {
-        console.error('nope not doing it', error)
-    }
-}
-main()
-// // Middleware
-// app.use(cors());
+// Middleware
+app.use(cors())
 // app.use(express.json());
 
-// // Test Route
-// app.get('/', (req, res) => {
-//     res.send('Backend is running...');
-// });
+// Test Route
+app.get('/', (req, res) => {
+    res.send('Backend is running...')
+    console.log("landed")
+})
 
-// // Query Route - Example: Fetch Tech Stacks
-// app.get('/api/test', (req, res) => {
-//     var con = db.connect()
-//     con.all('SELECT * FROM public.test', (err, rows) => {
-//         if (err) {
-//             return res.status(500).json({ error: err.message });
-//         }
-//         res.json(rows);
-//         });
-// });
+// Query Route - Example: Fetch Tech Stacks
+app.get('/api/test', async (req, res) => {
+    const test_res = await sequelize.query('SELECT * FROM public.test', { type: QueryTypes.SELECT })
+    res.json(test_res)
+    console.log(`fetched ${test_res.length} rows`)
+})
+            
+const port = process.env.PORT || 5000
 
-// // Start Server
-// app.listen(port, () => {
-//     console.log(`Server running on http://localhost:${port}`);
-// });
+// Start Server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+})
