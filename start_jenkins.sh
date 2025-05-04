1824a99d8b072a5c1a9bdde2256d1404417fd66e
Dongwon Henry Seon <tunacome@gmail.com>
2025-05-04 12:55:50 +0000

    test: Jenkins test

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -19,6 +19,7 @@ docker run -d --name jenkins \
     -p 8081:8080 -p 50000:50000 \
     -v "$VOL_NAME":/var/jenkins_home \
     -v /var/run/docker.sock:/var/run/docker.sock \
+    # -e JENKINS_PREFIX=/jenkins \
     --network deeverse_proxy \
     --restart=on-failure \
     jenkins/jenkins:lts-jdk17
