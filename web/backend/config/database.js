939d6f0e7d542e182a78dfb544e0476819e0b51a
Dongwon Henry Seon <tunacome@gmail.com>
2025-05-10 14:40:37 +0000

    Modify things so it works in production

--- web/backend/config/database.js
+++ web/backend/config/database.js
@@ -3,21 +3,21 @@ require("dotenv").config()
 
 const conn_info = {
     'host': process.env.FORT_PG_HOST,
-    'port': process.env.FORT_PG_PORT,
+    'port': Number(process.env.FORT_PG_PORT),
     'username': process.env.FORT_PG_USER,
     'password': process.env.FORT_PG_PASSWORD,
     'database': process.env.FORT_PG_DATABASE,
     'dialect': 'postgres'
 }
 
-const sequelize = new Sequelize(options=conn_info)
+const sequelize = new Sequelize(conn_info)
 
 const connCheck = async () => {
     try {
         await sequelize.authenticate();
-        console.log("DB connected.")
+        console.log("✅ DB connected.")
     } catch (error) {
-        console.log("DB connection failed:", error)
+        console.log("⛔ DB connection failed:", error)
     }
 }
 
--- web/backend/index.js
+++ web/backend/index.js
@@ -15,7 +15,7 @@ const stackRoutes = require("./routes/stackRoutes")
 const app = express()
 
 const path = require('path')
-app.use('/fortpolio', express.static(path.join(__dirname, '../client/dist')))
+app.use('/', express.static(path.join(__dirname, '../client/dist')))
 
 applyMiddlewares(app)
 app.use('/api/info', infoRoutes)
