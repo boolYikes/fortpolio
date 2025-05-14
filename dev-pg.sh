be5ba829252fcd6b26ed57c2a2f56c1d67915d13
Dee H Seon <tunacome@gmail.com>
2025-05-14 15:11:04 +0900

    Delete dev-pg.sh

--- dev-pg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -it --name fortpolio-postgres --rm -v fortpolio:/var/lib/postgresql/data -e POSTGRES_USER=fortpolio -e POSTGRES_PASSWORD=sdw5414 -e POSTGRES_DB=fortpolio -p 5433:5432 -d postgres:13
