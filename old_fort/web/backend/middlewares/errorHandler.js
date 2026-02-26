2593f9a2270595d149130d485d993ec206e4eee0
Dongwon Henry Seon <123602432+boolYikes@users.noreply.github.com>
2026-02-26 20:50:02 +0000

    Updates

--- old_fort/web/backend/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-    app.use((err, req, res, next) => {
-        console.error(err)
-        res.status(500).json({ error: err.message || "Internal Organ Error! 😫" })
-        next()
-    })
-    
-}
